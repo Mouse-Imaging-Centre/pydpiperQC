5e0bba6d922efd90f16ba54a52ff10e1cb1377fe
nzxwang <nickywang100@gmail.com>
2019-03-14 20:38:03 -0400

    pick the minc file for overlay; still need to default properly

--- server.R
+++ server.R
@@ -1,9 +1,14 @@
 server <- function(input, output) {
 
+  #TODO make this default properly
+  minc_overlay <- reactive({
+    input$minc_file %>% mincGetVolume() %>% mincArray()
+  })
+
   output$overlay <- renderPlot({
     sliceSeries(nrow=4, ncol=5, begin=100, end=300) %>%
       anatomy(consensus, low=low, high=high) %>%
-      contours(consensus, levels=c(1000,1400), col="red") %>%
+      contours(minc_overlay(), levels=c(1000,1400), col="red") %>%
       draw()
   })
 }
