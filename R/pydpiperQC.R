47c0c66d0c3b336a90b81d8b0e00620da93e27d0
nzxwang <nickywang100@gmail.com>
2019-04-05 19:01:37 -0400

    launch() function passes variables to shinyApp via .GlobalEnv

--- R/pydpiperQC.R
+++ R/pydpiperQC.R
@@ -1,7 +1,16 @@
 #'@importFrom magrittr %>%
 
 #' @export
-launch_app <- function() {
+launch <- function(annotation = NULL,
+                   consensus = NULL,
+                   wd=getwd()) {
+
+  .GlobalEnv$.annotation <- annotation
+  .GlobalEnv$.consensus <- consensus
+  .GlobalEnv$.wd <- wd
+
+  on.exit(rm(list=c(.annotation, .consensus, .wd), envir=.GlobalEnv))
+
   shiny::runApp(
     appDir = system.file('appDir', package = 'pydpiperQC'),
     launch.browser = TRUE
