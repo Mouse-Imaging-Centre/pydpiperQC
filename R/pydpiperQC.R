5376ad5505f2a1a66328668772822b00823ddc93
nzxwang <nickywang100@gmail.com>
2019-04-09 18:49:36 -0400

    download example data and let launch_example use it by default

--- R/pydpiperQC.R
+++ R/pydpiperQC.R
@@ -21,10 +21,28 @@ launch <- function(annotation = NULL,
 }
 
 #' @export
-launch_example <- function(data_dir) {
+launch_example <- function(data_dir=NULL) {
+
+  if (is.null(data_dir)){
+    data_dir <- tempdir()
+    download_example_data(data_dir)
+  }
   launch(
     annotation = file.path(data_dir, "tidy_analysis.csv"),
     consensus = file.path(data_dir, "test_nlin/test-nlin-3.mnc"),
-    wd = data_dir
   )
+}
+
+# changed from "wget" to stop freakouts on mac
+# this function largely borrowed from RMINC:::getRMINCTestData
+download_example_data <- function(data_path, method = "libcurl") {
+
+  downloadPath <- file.path(data_path, "pydpiperQC_data.tgz")
+
+  dir.create(data_path, showWarnings = FALSE, recursive = TRUE)
+  download.file("http://repo.mouseimaging.ca/repo/PydpiperQC_test-files/pydpiperQC_data.tgz",
+                destfile = downloadPath,
+                method = method)
+
+  untar(downloadPath, exdir = data_path, compressed = "gzip")
 }
